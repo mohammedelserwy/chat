50da4431a7e1506a390f0b6dbecdb17f8a4b0cd2
mohammedelserwy <80222232+mohammedelserwy@users.noreply.github.com>
2022-06-13 17:14:20 +0200

    Update App.js

--- App.js
+++ App.js
@@ -56,7 +56,7 @@ export default class Chat extends React.Component {
               <View
 
                 style={{
-                  alignSelf: "",
+                  
                   alignItems: "center",
                   width: 80,
                   height: 25,
